9e00bae1cb70a62c91c15e3c5ead9a350e3e62b3
Gino Hagad <ginohagad@gmail.com>
2015-10-06 14:16:59 +1300

    Updated and reuploaded all files to repo

--- www/js/app.js
+++ www/js/app.js
@@ -2,15 +2,14 @@ angular.module('pnrApp', ['ionic', 'pnrApp.controllers', 'pnrApp.filter', 'facto
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
+
       StatusBar.styleDefault();
     }
     // if(window.Connection){
@@ -73,25 +72,6 @@ angular.module('pnrApp', ['ionic', 'pnrApp.controllers', 'pnrApp.filter', 'facto
       }
     }
   });
-
-  //   .state('app.playlists', {
-  //     url: '/playlists',
-  //     views: {
-  //       'menuContent': {
-  //         templateUrl: 'templates/playlists.html',
-  //         controller: 'PlaylistsCtrl'
-  //       }
-  //     }
-  //   })
-  //
-  // .state('app.single', {
-  //   url: '/playlists/:playlistId',
-  //   views: {
-  //     'menuContent': {
-  //       templateUrl: 'templates/playlist.html',
-  //       controller: 'PlaylistCtrl'
-  //     }
-  //   }
-  // });
+  
   $urlRouterProvider.otherwise('/app/');
 });
